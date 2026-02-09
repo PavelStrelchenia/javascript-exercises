1d0e2d5277444f0d57d5510fefe4934652072982
Pavel Strelchenia <pivlik5608@gmail.com>
2026-02-09 23:51:18 +0300

    Completed 9 task

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+    if (a < 0 || b < 0 || !Number.isInteger(a) || !Number.isInteger(b))
+        return "ERROR";
 
+    if (a > b) {
+        const c = a;
+        a = b;
+        b = c;
+    }
+
+    let result = 0;
+    for (let i = a; i <= b; i++) {
+        result += i;
+    }
+    return result;
 };
 
 // Do not edit below this line
