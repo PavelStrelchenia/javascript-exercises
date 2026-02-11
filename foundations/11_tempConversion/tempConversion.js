f98a21d2e0a5c9acd4cb4914deda2a3865decd05
Pavel Strelchenia <pivlik5608@gmail.com>
2026-02-12 02:30:35 +0300

    Completed 11 task

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+    const convertTemp = ((temp - 32) * 5) / 9;
+    const roundTemp = Math.round(convertTemp * 10) / 10;
+    return roundTemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+    const convertTemp = (temp * 9) / 5 + 32;
+    const roundTemp = Math.round(convertTemp * 10) / 10;
+    return roundTemp;
 };
 
 // Do not edit below this line
 module.exports = {
-  convertToCelsius,
-  convertToFahrenheit
+    convertToCelsius,
+    convertToFahrenheit,
 };
